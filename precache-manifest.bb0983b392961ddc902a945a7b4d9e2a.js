dd7774528c5e920281084b342fd00905f518994d
gmarkosov <george32451@mail.ru>
2018-11-07 18:13:56 +0300

    Updates

---
+++ precache-manifest.bb0983b392961ddc902a945a7b4d9e2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "17a2bc672df91b8e40f7",
+    "url": "/redux-tutorial/static/js/runtime~main.17a2bc67.js"
+  },
+  {
+    "revision": "8a3621783ccc1e04f7fe",
+    "url": "/redux-tutorial/static/js/main.8a362178.chunk.js"
+  },
+  {
+    "revision": "fe76469d44066e91b116",
+    "url": "/redux-tutorial/static/js/1.fe76469d.chunk.js"
+  },
+  {
+    "revision": "8a3621783ccc1e04f7fe",
+    "url": "/redux-tutorial/static/css/main.5fee4080.chunk.css"
+  },
+  {
+    "revision": "3f23bd147023131cd2a84eaaa873b5a8",
+    "url": "/redux-tutorial/index.html"
+  }
+];
